4347de47b2aa258150597ea7f3cd6d454c09e9e8
Olga <olga.molchanova.olkamana@gmail.com>
2020-06-26 15:24:23 +0300

    screen created
    needs design

---
+++ app/src/main/java/com/example/ubercoffee/SmsCodeActivity.java
@@ -0,0 +1,15 @@
+package com.example.ubercoffee;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SmsCodeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sms_code);
+    }
+}
